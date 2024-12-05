45450de80aa916f3fe04f5c55ddf18c0b7939e22
dontMissChris <c-b-home@gmx.de>
2024-12-05 18:22:12 +0100

    Projektdateien hinzufügen.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Meine_Erinnerungs_app
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RemovePlaceholderText(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && (textBox.Text == "Grund" || textBox.Text == "Datum" || textBox.Text == "Uhrzeit"))
+            {
+                textBox.Text = "";
+                textBox.Foreground = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Colors.Black);
+            }
+        }
+
+        private void AddPlaceholderText(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                if (textBox.Name == "GrundTextBox")
+                {
+                    textBox.Text = "Grund";
+                }
+                else if (textBox.Name == "DatumTextBox")
+                {
+                    textBox.Text = "Datum";
+                }
+                else if (textBox.Name == "UhrzeitTextBox")
+                {
+                    textBox.Text = "Uhrzeit";
+                }
+                textBox.Foreground = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Colors.Gray);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("ERINNERUNG SCHARF GESTELLT");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            // Hier können Sie den Code zum Löschen der Erinnerung hinzufügen
+        }
+    }
+}
